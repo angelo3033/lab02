f5756276288bf5de73bf02f19ea8b2df72c2d2a5
angel samanez <angel.samanez@tecsup.edu.pe>
2021-03-24 20:13:34 -0500

    punto 5

--- src/Total.js
+++ src/Total.js
@@ -1,11 +1,13 @@
 import React from 'react'
 
 const Total = (props) => {
-  return(
-    <>
-      <p>Number of exercises {props.total}</p>
-    </>
-  )
+	const {part1, part2, part3} = props
+  	const total = part1.exercises + part2.exercises + part3.exercises
+  	return(
+	    <div>
+	    	<p>Number of exercises {total}</p>
+	    </div>  
+  	)
 }
 
 export default Total
